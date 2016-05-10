2a0775af083fa241966d72e6844d110b12b03c2e
mart-dominguez <mdomingu@gmail.com>
2016-05-10 14:03:48 -0300

    listo

--- scripts/services.js
+++ scripts/services.js
@@ -1,2 +1,17 @@
- angular.module('appEjemplo').factory('categoriaFactory',function(
- 	))
+angular.module("appLab09").factory("categoriaService",function(){
+	var catService={};
+	var categoria={id:undefined,nombre:undefined};
+	agendaService.listar=function(){
+		return agenda;
+	}
+	agendaService.buscarById=function(id){
+		return agenda;
+	}
+	agendaService.add(cat){
+		//
+	}
+	catServcice.nueva = function(){
+		return {id:undefined,nombre:undefined};
+	}
+	return agendaService;
+});
